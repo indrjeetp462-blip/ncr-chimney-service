33944514e440d55a978dcd6cf3cab0a7c841912b
Indrajeet singh <indrjeetp462@gmail.com>
2026-09-20 06:43:12 +0530

    Update sitemap.ts

--- sitemap.ts
+++ sitemap.ts
@@ -1,9 +1,1 @@
-import { MetadataRoute } from 'next'
-export default function sitemap(): MetadataRoute.Sitemap {
-  const base = 'https://ncr-chimney-service.vercel.app'
-  const locations = ['jaypee-greens','sector-150','raj-nagar-extension','crossing-republik','indirapuram','vaishali','vasundhara','gaur-city','noida-extension']
-  return [
-    { url: base, lastModified: new Date() },
-    ...locations.map((loc) => ({ url: `${base}/${loc}`, lastModified: new Date() }))
-  ]
-}
+
