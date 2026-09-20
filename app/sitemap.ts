d5c9cbed36bf1261b067ab5048ad829dbb8677d9
Indrajeet singh <indrjeetp462@gmail.com>
2026-09-20 06:59:28 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,10 +4,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const base = 'https://ncr-chimney-service.vercel.app'
   return [
     { url: base, lastModified: new Date() },
+    { url: base + '/faber-chimney-service-jaypee-greens-greater-noida', lastModified: new Date() },
+    { url: base + '/faber-chimney-service-vaishali-sector-5-ghaziabad', lastModified: new Date() },
+    { url: base + '/glen-chimney-service-indirapuram-ghaziabad', lastModified: new Date() },
+    { url: base + '/kaff-chimney-service-sector-150-noida', lastModified: new Date() },
     { url: base + '/jaypee-greens', lastModified: new Date() },
-    { url: base + '/sector-150', lastModified: new Date() },
-    { url: base + '/raj-nagar-extension', lastModified: new Date() },
-    { url: base + '/indirapuram', lastModified: new Date() },
     { url: base + '/vaishali', lastModified: new Date() },
   ]
 }
