273e7415c3ba7e6851765b69ff993278456c55a9
Indrajeet singh <indrjeetp462@gmail.com>
2026-09-22 18:35:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Chimney Service or repair near me in Noida Ghaziabad - Not Working Repair @Home",
-  description: "Best Chimney Service or repair near me  in Noida, Ghaziabad & Delhi NCR. Chimney Not Working? Same Day Repair, Cleaning & Service at Home. Call 8796284796",
+  title: "Chimney repair or service near me in Noida Ghaziabad - Not Working Repair @Home",
+  description: "Best Chimney repair or service near me  in Noida, Ghaziabad & Delhi NCR. Chimney Not Working? Same Day Repair, Cleaning & Service at Home. Call 8796284796",
 };
 
 export default function RootLayout({
